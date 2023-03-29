4b1c9809d31e3d439023886f46f68d2316da43c6
kamau-maina <mainkamjames@gmail.com>
2023-03-30 01:05:15 +0300

    completed task

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+ckage { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless => '/usr/bin/pip3 show flask | grep -w "Version: 2.1.0"',
+  require => Package['python3-pip'],
+}
+
